7da51b7f035a5d2bffe08b36f11aafd1590f8d05
tutuuuuuuuu-sudo <r2rgarraza@gmail.com>
2026-03-20 20:20:08 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,17 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     hmr: {
       overlay: false
+    },
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      }
     }
   },
   resolve: {
